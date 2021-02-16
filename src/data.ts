b95a55502a0cb30e777794e67cf7d31fb68a161c
Abdul Samad <Samk8778@yahoo.com>
2021-02-16 12:28:57 +0530

    :speech_balloon: Changed data

--- src/data.ts
+++ src/data.ts
@@ -1,411 +1,106 @@
-interface nameType {
-  title: string;
-  price: string;
-}
-
 /* Dummy Data: You can change this as per your demand */
 
-export const names: nameType[] = [
-  {
-    title: 'Tequila - Sauza Silver',
-    price: '$43.66',
-  },
-  {
-    title: 'Beef - Baby, Liver',
-    price: '$21.79',
-  },
-  {
-    title: 'Molasses - Fancy',
-    price: '$6.77',
-  },
-  {
-    title: 'Coffee - Flavoured',
-    price: '$77.42',
-  },
-  {
-    title: 'Wine - Kwv Chenin Blanc South',
-    price: '$43.33',
-  },
-  {
-    title: 'Strawberries - California',
-    price: '$48.94',
-  },
-  {
-    title: 'Pomello',
-    price: '$24.49',
-  },
-  {
-    title: 'Quail - Jumbo',
-    price: '$57.25',
-  },
-  {
-    title: 'Creme De Cacao White',
-    price: '$90.09',
-  },
-  {
-    title: 'Milk - 2%',
-    price: '$49.83',
-  },
-  {
-    title: 'Wine - Masi Valpolocell',
-    price: '$80.33',
-  },
-  {
-    title: 'Coffee - French Vanilla Frothy',
-    price: '$1.95',
-  },
-  {
-    title: 'Yeast Dry - Fermipan',
-    price: '$82.43',
-  },
-  {
-    title: 'Mushrooms - Honey',
-    price: '$51.43',
-  },
-  {
-    title: 'Muffin Chocolate Individual Wrap',
-    price: '$33.60',
-  },
-  {
-    title: 'Wine - Carmenere Casillero Del',
-    price: '$64.88',
-  },
-  {
-    title: 'Corn Kernels - Frozen',
-    price: '$72.91',
-  },
-  {
-    title: 'Shallots',
-    price: '$12.19',
-  },
-  {
-    title: 'Wine - Chateau Timberlay',
-    price: '$55.72',
-  },
-  {
-    title: 'Bread - Roll, Italian',
-    price: '$71.67',
-  },
-  {
-    title: 'Lamb - Whole, Fresh',
-    price: '$44.37',
-  },
-  {
-    title: 'Coconut - Shredded, Sweet',
-    price: '$87.63',
-  },
-  {
-    title: 'Salt - Rock, Course',
-    price: '$70.68',
-  },
-  {
-    title: 'Tarts Assorted',
-    price: '$30.71',
-  },
-  {
-    title: 'Cakes Assorted',
-    price: '$29.64',
-  },
-  {
-    title: 'Mop Head - Cotton, 24 Oz',
-    price: '$16.96',
-  },
-  {
-    title: 'Muskox - French Rack',
-    price: '$49.55',
-  },
-  {
-    title: 'Beans - Fine',
-    price: '$64.93',
-  },
-  {
-    title: 'Pork - Shoulder',
-    price: '$3.90',
-  },
-  {
-    title: 'Muffin Carrot - Individual',
-    price: '$51.34',
-  },
-  {
-    title: 'Icecream Cone - Areo Chocolate',
-    price: '$77.14',
-  },
-  {
-    title: 'Lettuce - Treviso',
-    price: '$87.49',
-  },
-  {
-    title: 'Juice - Grape, White',
-    price: '$16.33',
-  },
-  {
-    title: 'Basil - Pesto Sauce',
-    price: '$62.08',
-  },
-  {
-    title: 'Syrup - Monin - Granny Smith',
-    price: '$44.62',
-  },
-  {
-    title: 'Assorted Desserts',
-    price: '$70.46',
-  },
-  {
-    title: 'Wine - Cave Springs Dry Riesling',
-    price: '$71.42',
-  },
-  {
-    title: 'Bols Melon Liqueur',
-    price: '$77.30',
-  },
-  {
-    title: 'Doilies - 10, Paper',
-    price: '$18.87',
-  },
-  {
-    title: 'Yeast Dry - Fleischman',
-    price: '$70.71',
-  },
-  {
-    title: 'Cookie Trail Mix',
-    price: '$76.59',
-  },
-  {
-    title: 'Lettuce - Mini Greens, Whole',
-    price: '$73.83',
-  },
-  {
-    title: 'Milk Powder',
-    price: '$55.66',
-  },
-  {
-    title: 'Pepper - Orange',
-    price: '$65.10',
-  },
-  {
-    title: 'Milk 2% 500 Ml',
-    price: '$78.11',
-  },
-  {
-    title: 'Wine - Beringer Founders Estate',
-    price: '$58.04',
-  },
-  {
-    title: 'Wine - Red, Mouton Cadet',
-    price: '$57.96',
-  },
-  {
-    title: 'Mushroom - Chantrelle, Fresh',
-    price: '$77.70',
-  },
-  {
-    title: 'Lettuce - Boston Bib',
-    price: '$19.12',
-  },
-  {
-    title: 'Seaweed Green Sheets',
-    price: '$25.65',
-  },
-  {
-    title: 'Grouper - Fresh',
-    price: '$47.36',
-  },
-  {
-    title: 'Milk - 1%',
-    price: '$42.97',
-  },
-  {
-    title: 'Roe - White Fish',
-    price: '$50.53',
-  },
-  {
-    title: 'Tomatoes - Orange',
-    price: '$34.21',
-  },
-  {
-    title: 'Juice - Pineapple, 341 Ml',
-    price: '$91.12',
-  },
-  {
-    title: 'Oil - Shortening,liqud, Fry',
-    price: '$44.11',
-  },
-  {
-    title: 'Cleaner - Bleach',
-    price: '$88.95',
-  },
-  {
-    title: 'Quail - Whole, Boneless',
-    price: '$75.81',
-  },
-  {
-    title: 'Gatorade - Orange',
-    price: '$40.49',
-  },
-  {
-    title: 'Cookies Oatmeal Raisin',
-    price: '$43.32',
-  },
-  {
-    title: 'Cod - Fillets',
-    price: '$35.38',
-  },
-  {
-    title: 'Chickhen - Chicken Phyllo',
-    price: '$50.68',
-  },
-  {
-    title: 'Pork - Ham Hocks - Smoked',
-    price: '$22.95',
-  },
-  {
-    title: 'Cheese - Cheddar With Claret',
-    price: '$81.31',
-  },
-  {
-    title: 'Flour - Cake',
-    price: '$75.38',
-  },
-  {
-    title: 'Lid - 16 Oz And 32 Oz',
-    price: '$4.80',
-  },
-  {
-    title: 'Pepperoni Slices',
-    price: '$61.26',
-  },
-  {
-    title: 'Lamb - Shoulder',
-    price: '$1.74',
-  },
-  {
-    title: 'Soap - Pine Sol Floor Cleaner',
-    price: '$58.28',
-  },
-  {
-    title: 'Sugar - Icing',
-    price: '$79.45',
-  },
-  {
-    title: 'Muffin Carrot - Individual',
-    price: '$61.06',
-  },
-  {
-    title: 'Tea - Decaf 1 Cup',
-    price: '$70.70',
-  },
-  {
-    title: 'Puff Pastry - Sheets',
-    price: '$69.42',
-  },
-  {
-    title: 'Sausage - Blood Pudding',
-    price: '$46.23',
-  },
-  {
-    title: 'Wine - Red, Lurton Merlot De',
-    price: '$71.94',
-  },
-  {
-    title: 'Lobster - Live',
-    price: '$70.35',
-  },
-  {
-    title: 'Beef - Roasted, Cooked',
-    price: '$51.79',
-  },
-  {
-    title: 'Zucchini - Yellow',
-    price: '$49.87',
-  },
-  {
-    title: 'Lamb - Ground',
-    price: '$67.63',
-  },
-  {
-    title: 'Containter - 3oz Microwave Rect.',
-    price: '$95.58',
-  },
-  {
-    title: 'Almonds Ground Blanched',
-    price: '$64.10',
-  },
-  {
-    title: 'Wine - Lamancha Do Crianza',
-    price: '$81.12',
-  },
-  {
-    title: 'Food Colouring - Red',
-    price: '$13.71',
-  },
-  {
-    title: 'Curry Paste - Green Masala',
-    price: '$52.52',
-  },
-  {
-    title: 'Figs',
-    price: '$27.09',
-  },
-  {
-    title: 'Soup - Campbellschix Stew',
-    price: '$35.62',
-  },
-  {
-    title: 'Lambcasing',
-    price: '$91.07',
-  },
-  {
-    title: 'Island Oasis - Ice Cream Mix',
-    price: '$87.57',
-  },
-  {
-    title: 'Ecolab - Orange Frc, Cleaner',
-    price: '$94.12',
-  },
-  {
-    title: 'Puree - Pear',
-    price: '$4.68',
-  },
-  {
-    title: 'Pepper Squash',
-    price: '$55.67',
-  },
-  {
-    title: 'Sauerkraut',
-    price: '$59.13',
-  },
-  {
-    title: 'Thyme - Dried',
-    price: '$63.73',
-  },
-  {
-    title: 'Muffin Mix - Morning Glory',
-    price: '$71.17',
-  },
-  {
-    title: 'Sauce - Alfredo',
-    price: '$76.08',
-  },
-  {
-    title: 'Coffee Cup 16oz Foam',
-    price: '$14.61',
-  },
-  {
-    title: 'Pop Shoppe Cream Soda',
-    price: '$99.29',
-  },
-  {
-    title: 'Cocoa Feuilletine',
-    price: '$96.23',
-  },
-  {
-    title: 'Bar - Sweet And Salty Chocolate',
-    price: '$20.28',
-  },
-  {
-    title: 'Sausage - Breakfast',
-    price: '$63.39',
-  },
+export const names: string[] = [
+  'Water - Spring 1.5lit',
+  'Crab - Soft Shell',
+  'Wine - Domaine Boyar Royal',
+  'Sea Bass - Whole',
+  'Mix - Cocktail Ice Cream',
+  'Cheese - Cheddar, Mild',
+  'Bread - English Muffin',
+  'Puree - Blackcurrant',
+  'Maintenance Removal Charge',
+  'Coffee - Espresso',
+  'Pork - Smoked Back Bacon',
+  'Chocolate Bar - Oh Henry',
+  'Dry Ice',
+  'Pasta - Canelloni',
+  'Eggroll',
+  'Wine - Chateau Timberlay',
+  'Wine - White, Ej',
+  'Tea Leaves - Oolong',
+  'Wine - Champagne Brut Veuve',
+  'Cookies Oatmeal Raisin',
+  'Egg Patty Fried',
+  'Ice Cream - Fudge Bars',
+  'Onion - Dried',
+  'Foil - 4oz Custard Cup',
+  'Tendrils - Baby Pea, Organic',
+  'Duck - Breast',
+  'Salmon - Smoked, Sliced',
+  'Quail - Whole, Bone - In',
+  'Table Cloth 62x114 White',
+  'Bar Mix - Lemon',
+  'Bok Choy - Baby',
+  'Chips Potato Salt Vinegar 43g',
+  'Cookie Dough - Peanut Butter',
+  'Beef - Kindney, Whole',
+  'Muffin Chocolate Individual Wrap',
+  'Coffee - Decafenated',
+  'Wine - Jaboulet Cotes Du Rhone',
+  'Rum - Dark, Bacardi, Black',
+  'Truffle Shells - Semi - Sweet',
+  'Dried Apple',
+  'Crackers - Trio',
+  'Ice Cream - Chocolate',
+  'Pasta - Fettuccine, Dry',
+  'Asparagus - White, Canned',
+  'Irish Cream - Baileys',
+  'Southern Comfort',
+  'Pork - Backs - Boneless',
+  'Pork Ham Prager',
+  'Lettuce - Iceberg',
+  'Spinach - Spinach Leaf',
+  'Pasta - Cheese / Spinach Bauletti',
+  'Beets - Candy Cane, Organic',
+  'Pepper - Sorrano',
+  'Longos - Burritos',
+  'Sugar - Sweet N Low, Individual',
+  'Water - Green Tea Refresher',
+  'Sprouts Dikon',
+  'Beef - Rib Roast, Cap On',
+  'Juice - Propel Sport',
+  'Butter Ripple - Phillips',
+  'Veal - Ground',
+  'Sauce - Thousand Island',
+  'Veal - Slab Bacon',
+  'Pork - Side Ribs',
+  'Mushroom - Trumpet, Dry',
+  'Wine - Prem Select Charddonany',
+  'Celery Root',
+  'Coconut - Shredded, Unsweet',
+  'Amarula Cream',
+  'Cheese - Brie, Triple Creme',
+  'Coffee Cup 12oz 5342cd',
+  'Artichoke - Bottom, Canned',
+  'Tomatoes - Heirloom',
+  'Island Oasis - Lemonade',
+  'Cheese Cloth No 100',
+  'Wine - Pinot Noir Mondavi Coastal',
+  'Bread - English Muffin',
+  'Vaccum Bag - 14x20',
+  'Mustard - Individual Pkg',
+  'Lobster - Canned Premium',
+  'Fish - Bones',
+  'Milk - 1%',
+  'Chicken Thigh - Bone Out',
+  'Sesame Seed Black',
+  'Devonshire Cream',
+  'Rice Paper',
+  'Lettuce - Lambs Mash',
+  'Squid - Breaded',
+  'Isomalt',
+  'Tea - Grapefruit Green Tea',
+  'Sauce - Soya, Light',
+  'Gelatine Leaves - Envelopes',
+  'Flower - Dish Garden',
+  'Wine - Vouvray Cuvee Domaine',
+  'Veal - Insides Provini',
+  'Fiddlehead - Frozen',
+  'Veal - Eye Of Round',
+  'Prunes - Pitted',
+  'Pasta - Orecchiette',
+  'Eggplant - Asian',
 ];
 
 // Material UI only supports string type in autocomplete options
