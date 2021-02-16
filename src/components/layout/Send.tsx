8701d2416c4a3ee52dfce74ff96bc7eb72fc97f3
Abdul Samad <Samk8778@yahoo.com>
2021-02-16 14:04:33 +0530

    Added send component

--- src/components/layout/Send.tsx
+++ src/components/layout/Send.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import WhatsApp from '../messenger/WhatsApp';
 
 const useStyles = makeStyles({
   root: {
     marginTop: 15,
+  },
+  row: {
+    display: 'flex',
+    gap: 15,
+    flexWrap: 'wrap',
   },
 });
 
@@ -14,6 +20,9 @@ const Send: FC = () => {
   return (
     <Container className={classes.root}>
       <h3>Send via -</h3>
+      <div className={classes.row}>
+        <WhatsApp />
+      </div>
     </Container>
   );
 };
